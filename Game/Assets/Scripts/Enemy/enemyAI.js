574ea42547d5a66fb772ec3d4918ea81461308bd
Schlagenhauf <44496@psdschools.org>
2016-05-18 14:39:17 -0600

    Enemies and more puzzles

---
+++ Game/Assets/Scripts/Enemy/enemyAI.js
@@ -0,0 +1,64 @@
+﻿#pragma strict
+
+var rand : int;
+var speed : float;
+var vertical : System.Boolean;
+
+function Start () {
+
+
+}
+/*
+	if (rand == 1){
+		GetComponent.<Rigidbody2D>().velocity.y = speed;
+		GetComponent.<Rigidbody2D>().velocity.x = 0;
+	}
+	else if (rand == 2){
+		GetComponent.<Rigidbody2D>().velocity.y = 0;
+		GetComponent.<Rigidbody2D>().velocity.x = speed;
+	}
+	else if (rand == 3){
+		GetComponent.<Rigidbody2D>().velocity.y = speed * -1;
+		GetComponent.<Rigidbody2D>().velocity.x = 0;
+	}
+	else if (rand == 4){
+		GetComponent.<Rigidbody2D>().velocity.y = 0;
+		GetComponent.<Rigidbody2D>().velocity.x = speed * -1;
+	}
+*/
+
+
+function Update () {
+	rand = Random.Range(1,3);
+	if (!vertical){
+		if (GetComponent.<Rigidbody2D>().velocity.x == 0){
+			if (rand == 1){
+				GetComponent.<Rigidbody2D>().velocity.x = speed;
+			}
+			else if (rand == 2){
+				GetComponent.<Rigidbody2D>().velocity.x = speed * -1;
+			}
+		}
+	}
+	else {
+		if (GetComponent.<Rigidbody2D>().velocity.y == 0){
+			if (rand == 1){
+				GetComponent.<Rigidbody2D>().velocity.y = speed;
+			}
+			else if (rand == 2){
+				GetComponent.<Rigidbody2D>().velocity.y = speed * -1;
+			}
+		}
+	}
+
+}
+/*
+function OnCollisonEnter2D (coll 	: Collider2D) {
+	Debug.Log ("test");
+	if (coll.gameObject.tag == "Obs"){
+		
+		GetComponent.<Rigidbody2D>().velocity.y = GetComponent.<Rigidbody2D>().velocity.y *-1;
+		GetComponent.<Rigidbody2D>().velocity.x = GetComponent.<Rigidbody2D>().velocity.x *-1;
+	}
+}
+*/
---
+++ Game/Assets/Scripts/Puzzle Objects/seperationWall.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var player : GameObject;
+
+function Start () {
+Physics2D.IgnoreCollision(player.GetComponent.<Collider2D>(), GetComponent.<Collider2D>());
+
+}
+
+function Update () {
+
+}
